c786d81fddf3ef639ad1de80169ce90e2f9d4cfc
luc-gomes <lucasgomes-rp@Outlook.com.br>
2022-12-20 21:25:10 -0300

    main

--- main/views.py
+++ main/views.py
@@ -4,3 +4,7 @@ from django.http import HttpResponse
 
 def index(response):
     return HttpResponse("<h1> texto texto texto</h1>")
+
+def v1(response):
+    return HttpResponse("<h1>222 texto</h1>")
+
