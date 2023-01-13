2a4b44819b2dc7c7b8ea092818efc6d712de3937
luc-gomes <lucasgomes-rp@Outlook.com.br>
2023-01-13 12:53:06 -0300

    bootstrap

--- main/urls.py
+++ main/urls.py
@@ -4,10 +4,13 @@ from . import views
 
 
 urlpatterns = [
-   # path ("", views.index, name="index"),
-   # path ("v1/", views.v1, name="view1"),
+    #path ("", views.index, name="index"),
+    #path ("v1/", views.v1, name="view1"),
     path("<int:id>", views.index, name="index"),
-    path("", views.home, name = "home"),
+    path("home/", views.home, name = "home"),
     path("create/", views.cadastro, name = "cadastro"),#formulario de cadastro
+    path("2/", views.cadastro, name = "list")
+
+
 
 ]
